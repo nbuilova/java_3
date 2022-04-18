1ef76fc302027f1885153830b7785dfd86b1f915
NBuilova <chashnikova.n@gmail.com>
2022-04-18 20:42:20 +0600

    Java 3 - hw 5

---
+++ src/lesson_5_hw/Race.java
@@ -0,0 +1,13 @@
+package lesson_5_hw;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Race {
+    private ArrayList<Stage> stages;
+    public ArrayList<Stage> getStages() { return stages; }
+    public Race(Stage... stages) {
+        this.stages = new ArrayList<>(Arrays.asList(stages));
+    }
+}
+
